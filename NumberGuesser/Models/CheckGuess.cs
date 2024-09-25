422ba9141a17f57c8fd431cbe811ca4d0eda531b
unknown <suraj.sakhare5869@gmail.com>
2024-09-25 19:55:45 +0530

    Added

---
+++ NumberGuesser/Models/CheckGuess.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NumberGuesser.Models
+{
+    internal class CheckGuess
+    {
+        public static string GuessNumber(int number, int guess)
+        {
+            if (guess == number)
+            {
+                return "correct";
+            }
+            else if(guess < number)
+            {
+                return "Too low";
+            }
+            else 
+            {
+                return "Too high";
+            }
+        }
+    }
+}
---
+++ NumberGuesser/Program.cs
@@ -0,0 +1,46 @@
+﻿using NumberGuesser.Models;
+
+namespace NumberGuesser
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int maxChances = 10;
+            int chance = 0;
+            bool win = false;
+            int lowerBound = 1;
+            int upperBound = 100;
+
+            Console.WriteLine("Welcome to the Number guessing game");
+
+            Random random = new Random();
+            int number = random.Next(lowerBound, upperBound);
+
+            while(maxChances!=chance)
+            {
+                Console.WriteLine($"Guess the Number within {lowerBound} to {upperBound}: ");
+                int guess = int.Parse(Console.ReadLine());
+
+                string output = CheckGuess.GuessNumber(number, guess);
+                chance++;
+
+                if (output == "correct")
+                {
+                    Console.WriteLine($"Congratulation you guessed the correct number in {chance} chances");
+                    win = true;
+                    break;
+                }
+                else
+                {
+                    Console.WriteLine($"{output}. Try again\n");
+                }
+            }
+
+            if (!win) {
+                Console.WriteLine($"Sorry you ran out of the chances, the number is {number}");
+            }
+
+        }
+    }
+}
